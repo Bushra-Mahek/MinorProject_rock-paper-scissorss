42caea75ae54ed32e7206c29e9af0b91db23a9af
Bushra-Mahek <bushramahek1013@gmail.com>
2025-05-24 22:21:43 +0530

    rps

---
+++ index.js
@@ -0,0 +1,241 @@
+// let score={wins:0,loses:0,ties:0};
+      const score = JSON.parse(localStorage.getItem('score'))||{wins:0,loses:0,ties:0};
+
+
+      
+      displayYourResult();
+      displayResult();
+      displayOnWebPage();
+      
+     
+     
+      function play(user){
+      let computer = ' ';
+      var n = Math.random();
+      let result = '';
+      
+
+      if(n <= 0.3){
+        computer="rock";
+      }
+      else if(n > 0.3 && n<= 0.7){
+        computer = "paper";
+      }
+      else{
+        computer="scissors";
+      }
+
+      
+
+    
+    //   if(user == 'rock'){
+    //   if(computer == 'scissor'){
+    //     result = 'win';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    //   else if(computer == 'paper'){
+    //     result = 'lose';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    //   else{
+    //     result = 'tie';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    // }
+
+    // else if(user == 'paper'){
+    //   if(computer == 'scissor'){
+    //     result = 'lose';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    //   else if(computer == 'rock'){
+    //     result = 'win';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    //   else{
+    //     result = 'tie';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    // }
+    // else{
+    //     if(computer == 'scissor'){
+    //       result = 'tie';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    //   else if(computer == 'rock'){
+    //     result = 'lose';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    //   else{
+    //     result = 'win';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    // }
+    if(user == computer){
+      score.ties++;
+      result = 'tie';
+    }
+    else if((user == 'rock' && computer == 'scissors') || (user == 'paper' && computer == 'rock')||(user == 'scissors' && computer =='paper')){
+      score.wins++;
+      result = ' you win';
+    }
+    else{
+      score.loses++;
+      result = 'you lose';
+    }
+
+    // localStorage.setItem('score',JSON.stringify(score));
+    localStorage.setItem('score',JSON.stringify(score));
+    
+    displayYourResult(result);
+    displayResult(user,computer);
+    displayOnWebPage();
+
+    alert(`your result: ${result}\n you are ${user} computer is ${computer}\n your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`)
+
+
+  };
+  function displayYourResult(result){
+        document.querySelector('.result').innerHTML=`${result}`;
+   }
+    function displayResult(user,computer){
+        document.querySelector('.moveplayed').innerHTML = `you are <img src="images/${user}.png" class="move-icon"> 
+        <img class="move-icon" src="images/${computer}.png"> is computer`;
+     }
+
+  function displayOnWebPage(){
+         document.querySelector('.scoree').innerHTML=`wins:${score.wins} loses:${score.loses} ties:${score.ties}`;
+    }
+  function reset(){
+    score.wins = 0;
+    score.loses = 0;
+    score.ties = 0;
+    localStorage.removeItem('score');
+  }
+   displayYourResult('');
+    displayResult('', '');
+    displayOnWebPage();
+
+
+    let var2 = Math.random();
+    if(var2 < 0.3){
+      computer2 = 'rock';
+    }
+    else if(var2 >= 0.3 && var2 < 0.7){
+      computer2 = 'paper';
+    }
+
+    else{
+      computer2 = 'scissors';
+    }
+
+    function play2(user){
+      let computer = ' ';
+      var n = Math.random();
+      let result = '';
+      
+
+      if(n <= 0.3){
+        computer="rock";
+      }
+      else if(n > 0.3 && n<= 0.7){
+        computer = "paper";
+      }
+      else{
+        computer="scissors";
+      }
+
+      
+
+    
+    //   if(user == 'rock'){
+    //   if(computer == 'scissor'){
+    //     result = 'win';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    //   else if(computer == 'paper'){
+    //     result = 'lose';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    //   else{
+    //     result = 'tie';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    // }
+
+    // else if(user == 'paper'){
+    //   if(computer == 'scissor'){
+    //     result = 'lose';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    //   else if(computer == 'rock'){
+    //     result = 'win';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    //   else{
+    //     result = 'tie';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    // }
+    // else{
+    //     if(computer == 'scissor'){
+    //       result = 'tie';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    //   else if(computer == 'rock'){
+    //     result = 'lose';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    //   else{
+    //     result = 'win';
+    //     alert(`you ${result} computer is ${computer} your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`);
+    //   }
+    // }
+    if(user == computer){
+      score.ties++;
+      result = 'tie';
+    }
+    else if((user == 'rock' && computer == 'scissors') || (user == 'paper' && computer == 'rock')||(user == 'scissors' && computer =='paper')){
+      score.wins++;
+      result = ' you win';
+    }
+    else{
+      score.loses++;
+      result = 'you lose';
+    }
+
+    // localStorage.setItem('score',JSON.stringify(score));
+    localStorage.setItem('score',JSON.stringify(score));
+    
+    displayYourResult(result);
+    displayResult(user,computer);
+    displayOnWebPage();
+
+    // alert(`your result: ${result}\n you are ${user} computer is ${computer}\n your score:\n wins:${score.wins} loses:${score.loses} ties:${score.ties}`)
+
+
+  };
+    
+    let intervalId;
+    function autoplay(){
+      console.log("Playing..");
+    intervalId = setInterval(()=>play2(computer2),3000);
+    }
+    function autostop(){
+      clearInterval(intervalId);
+    }
+    let isPlaying=false;
+
+    function autoPlayToggle(be){
+      if(!isPlaying){
+        autoplay();
+        be.textContent = "Stop Autoplaying";}
+      else{
+        autostop();
+        be.textContent = "AutoPlay";
+      }
+
+      isPlaying = !isPlaying;
+    };
+
+  
